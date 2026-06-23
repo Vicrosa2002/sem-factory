9811399c81a6999b933a600c4269e105e662312c
Vicrosa2002 <vicrosa1990@gmail.com>
2026-06-23 17:22:55 -0600

    conexion a db

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,11 @@
 import path from "node:path"
 import { defineConfig } from "prisma/config"
 
+const url = process.env.DIRECT_URL ?? ""
+
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
+  datasource: {
+    url,
+  },
 })
